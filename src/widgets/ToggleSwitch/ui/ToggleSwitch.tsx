b75702061cb086c45d5812f9ca39061108a37f28
Petr <ptrklsnkv@gmail.com>
2022-11-05 20:43:25 +0300

    svg loader, file loader, upgrade switch toggle

--- src/widgets/ToggleSwitch/ui/ToggleSwitch.tsx
+++ src/widgets/ToggleSwitch/ui/ToggleSwitch.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
-import { Switch } from 'antd';
+import React, { FC } from 'react'
 import cls from './style.module.scss'
+import { useTheme } from 'app/providers/ThemeProvider'
+import { Theme } from 'app/providers/ThemeProvider/lib/ThemeContext'
+import LightIcon from 'shared/assets/icons/Light.svg'
+import NightIcon from 'shared/assets/icons/Night.svg'
+import { Button, ThemeButton } from 'shared/ui/Button/Button'
+import { classNames } from 'shared/lib/classNames/classNames'
 
 interface IToggleProps {
-  toggleTheme: () => void
-  isActive: boolean
-  children: string
+  className?: string
 }
-export const ToggleSwitch = ({toggleTheme, children, isActive}:IToggleProps) => {
+export const ToggleSwitch: FC<IToggleProps> = ({className}: IToggleProps) => {
+  const {theme, toggleTheme} = useTheme()
 
   return (
-    <div className={cls.ToggleSwitch}>
-      <p className={cls.children}>{children}</p>
-      <Switch
-        checkedChildren={<CheckOutlined />}
-        unCheckedChildren={<CloseOutlined />}
-        defaultChecked={isActive}
-        onChange={toggleTheme}
-      />
-    </div>
+    <Button className={classNames(cls.ToggleSwitch, {}, [className])} onClick={toggleTheme}  theme={ThemeButton.CLEAR}>
+      {theme === Theme.DARK ? <LightIcon/> : <NightIcon/>}
+    </Button>
   )
 };
