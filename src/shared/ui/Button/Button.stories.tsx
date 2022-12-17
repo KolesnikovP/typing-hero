d0a604ed6f079f9b6f9d77de5c02d742c0759a97
Petr <ptrklsnkv@gmail.com>
2022-12-18 00:40:18 +0300

    TS-51 SB decorators

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,27 +1,55 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 import { Button, ThemeButton } from './Button';
 
-export default { };
+export default {
+  title: 'shared/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
 interface ButtonProps {
   className?: string
   theme?: ThemeButton
 }
 
-const Template = (args: ButtonProps & {children: ReactNode}) => (
-  // 👇 Your template goes here
-  <Button {...args}>test</Button>
-);
+// const Template = (args: ButtonProps & {children: ReactNode}) => (
+//   // 👇 Your template goes here
+//   <Button {...args}>test</Button>
+// );
 
 export const Primary = Template.bind({});
 Primary.args = {
-  variant: 'primary',
+  children: 'Text',
 };
+Primary.decorators = [ThemeDecorator(Theme.DARK)];
 
 export const Clear = Template.bind({});
 Clear.args = {
-  variant: 'secondary',
+  children: 'Text',
   theme: ThemeButton.CLEAR,
 };
+Clear.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const Outline = Template.bind({});
+Outline.args = {
+  children: 'Text',
+  theme: ThemeButton.OUTLINE,
+};
+Outline.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const OutlineLight = Template.bind({});
+OutlineLight.args = {
+  children: 'Text',
+  theme: ThemeButton.OUTLINE,
+};
+OutlineLight.decorators = [ThemeDecorator(Theme.LIGHT)];
 
 /**
  * Primary UI component for user interaction
