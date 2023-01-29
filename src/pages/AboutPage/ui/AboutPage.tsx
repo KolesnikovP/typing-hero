46b28938511cbd0a546d8576ab99bdc45210034c
Petr <ptrklsnkv@gmail.com>
2023-01-29 23:37:12 +0600

    TS-71 add new custom input, add feature login, also do better performance in modal (lazy)

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BugButton } from 'app/providers/ErrorBoundary';
 import { Counter } from 'entities/Counter';
+import { Input } from 'shared/ui/Input/Input';
 
 const AboutPage = () => {
   const { t } = useTranslation('about');
+  const [value, setValue] = useState('uoh');
+
+  const onChange = (val: string): void => {
+    setValue(val);
+  };
 
   return (
     <div>
@@ -14,6 +20,7 @@ const AboutPage = () => {
         <BugButton />
       </div>
       <Counter />
+      <Input value={value} onChange={onChange} />
     </div>
   );
 };
