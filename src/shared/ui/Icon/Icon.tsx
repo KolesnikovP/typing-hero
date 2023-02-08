0713154618bd52c1d36a8bd69b7bbb987ad65953
Petr <ptrklsnkv@gmail.com>
2023-02-08 23:02:59 +0600

    TS-85 shared component for svg

--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -1,21 +1,44 @@
-import { ReactNode, SVGProps, ReactElement } from 'react';
-import ThemeIcon from 'shared/assets/icons/Light.svg';
-// import cls from './Icon.module.scss';
+import { ReactNode } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import HomeIconSVG from '../../assets/icons/HomeIcon.svg';
+import LightIconSVG from '../../assets/icons/Light.svg';
 
-export enum IconType {
-  themeIcon = 'themeIcon',
+import styles from './Icon.module.scss';
+
+export enum IconName {
+  HOME_ICON = 'homeIcon',
+  LIGHT_ICON = 'lightIcon'
 }
 
-interface IconProps extends SVGProps<SVGSVGElement> {
-  type: IconType
-  size?: number
-}
+const icons: Record<IconName, ReactNode> = {
+  homeIcon: <HomeIconSVG />,
+  lightIcon: <LightIconSVG />,
+};
+
+type IconProps = {
+  type: IconName;
+  color?: string;
+  transform?: 'rotate90' | 'rotate180';
+  className?: string;
+};
 
 export const Icon = (props: IconProps) => {
-  const { type, size = 20 } = props;
-
+  const {
+    color, transform, type, className,
+  } = props;
   return (
-  // {type === IconType.themeIcon && <ThemeIcon fontSize={size} />}
-    <span>do it</span>
+    <span
+      style={{ color }}
+      className={classNames(
+        styles.icon,
+        {
+          [styles.icon_rotate90]: transform === 'rotate90',
+          [styles.icon_rotate180]: transform === 'rotate180',
+        },
+        [className],
+      )}
+    >
+      {icons[type]}
+    </span>
   );
 };
